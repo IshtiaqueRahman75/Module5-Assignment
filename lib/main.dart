d61e9f5448f1d8eee5464e688a5fefc1fe564d05
ishtiaque.rahman <ishtiaque.rahman@mgi.org>
2025-06-25 23:38:52 +0600

    assignment commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
-import 'package:flutter/material.dart';
-
 // a) Abstract class Vehicle
-void main() {
-  List<Map<String, dynamic>> students = [
-    {"name": "Alice", "scores": [85, 90, 78]},
-    {"name": "Bob", "scores": [88, 76, 95]},
-    {"name": "Charlie", "scores": [90, 92, 85]}
-  ];
-  Map<String, double> studentAverages = {};
-  for (var student in students) {
-    String name = student["name"];
-    List<int> scores = List<int>.from(student["scores"]);
-    double average = scores.reduce((sum, score) => sum + score) / scores.length;
-    studentAverages[name] = double.parse(average.toStringAsFixed(2));
+abstract class Vehicle {
+  int _speed = 0; // c) Encapsulated variable
+
+  void setSpeed(int speed) {
+    _speed = speed;
   }
-  var sortedAverages = Map.fromEntries(
-      studentAverages.entries.toList()
-        ..sort((a, b) => b.value.compareTo(a.value))
-  );
-  print("{");
-  sortedAverages.forEach((name, average) {
-    print('  "$name": $average,');
-  });
-  print("}");
+
+  void move(); 
 }
 
+// b) Subclass Car
+class Car extends Vehicle {
+  @override
+  void move() {
+    print("The car is moving at $_speed km/h");
+  }
+}
 
+// d) Main function
+void main() {
+  Car myCar = Car();
+  myCar.setSpeed(80);
+  myCar.move();
+}
